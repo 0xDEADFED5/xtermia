3aaeb07b79e99c799d6efba44e8083c61f8bd9c5
0xDEADFED5 <admin@terminoid.com>
2025-03-11 03:20:30 -0700

    update xterm.js

--- web/static/webclient/js/webclient.js
+++ web/static/webclient/js/webclient.js
@@ -1,4 +1,4 @@
-const revision = 132;
+const revision = 133;
 const font = new FontFaceObserver('Fira Code');
 font.load().then(() => {
     console.log('Font loaded.');
@@ -911,7 +911,7 @@ font.load().then(() => {
                 }
                 currentLine += char;
                 visibleLength++;
-                if (visibleLength >= maxWidth - 1) {
+                if (visibleLength > maxWidth) {
                     let wrapIndex = -1;
                     // Find last space within the *visible* width
                     for (let k = currentLine.length - 1; k >= 0; k--) {
